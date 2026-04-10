af0dd516e49ae8f650acef4c1ca26902a8775b6d
User Aitalina <aitautau@gmail.com>
2026-04-10 16:37:00 +0500

    Checkstyle

--- stats-service/stats-server/src/main/java/ru/practicum/service/StatsService.java
+++ stats-service/stats-server/src/main/java/ru/practicum/service/StatsService.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public interface StatsService {
     void saveHit(EndpointHitDto hitDto);
+
     List<ViewStatsDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique);
 
 }
